9a7e82de717421af2c26c7d1b7a631c80bfa7816
KoiKomei <32816781+KoiKomei@users.noreply.github.com>
2018-12-05 11:33:32 +0100

    Tolto del codice residuo

--- Elevator/Main.cpp
+++ Elevator/Main.cpp
@@ -6,8 +6,6 @@ int main(){
 	cout << "Hello world!"<<endl;
 	sf::RenderWindow window(sf::VideoMode(1280, 720), "SFML works!");
 	Context cont(window);
-	sf::CircleShape shape(100.f);
-	shape.setFillColor(sf::Color::Green);
 	cont.changer(window);
 	window.close();
 
